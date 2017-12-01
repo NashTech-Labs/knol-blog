83b677ba4d08aa05d19c69eea0bd69f0014f309d
Akshansh Jain <akshansh.jain@knoldus.in>
2017-12-01 10:18:28 +0530

    Seperated Service, controller, Boot and utility

---
+++ src/main/scala/Boot.scala
@@ -0,0 +1,16 @@
+import akka.actor.{ActorSystem, _}
+import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
+import controllers.{ProcessBlogsView, WordpressController}
+import net.liftweb.json.{parse => liftParse}
+import services.WordpressService
+
+object Boot extends App {
+  implicit val system = ActorSystem("Blogger-Scheduler")
+
+  val blogScheduler = system.actorOf(Props(new WordpressController()))
+
+  val scheduler = QuartzSchedulerExtension(system)
+
+  scheduler.schedule("EveryMonth", blogScheduler, ProcessBlogsView)
+
+}
