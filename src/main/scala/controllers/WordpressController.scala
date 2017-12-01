03fc2418c648fd8cb8c0670d4887c649bcbabb6d
Akshansh Jain <akshansh.jain@knoldus.in>
2017-12-01 10:05:39 +0530

    Broke received method into postMessageOnSlack and getFinalResult

--- src/main/scala/controllers/WordpressController.scala
+++ src/main/scala/controllers/WordpressController.scala
@@ -1,15 +1,14 @@
 package controllers
 
-import java.text.SimpleDateFormat
-import java.util.Calendar
-
 import akka.actor.{Actor, _}
 import net.liftweb.json.{parse => liftParse, _}
-import services.WordpressService
+import services.{Post, WordpressService}
 import slack.api.SlackApiClient
+import utilities.CalendarHelper
 import utilities.ConfigHelper._
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 case class FormattedBlogger(name: String, numberOfBlogs: Int, totalViews: Int)
 
@@ -25,25 +24,11 @@ class WordpressController(implicit val system: ActorSystem) extends Actor with A
 
   def receive: Receive = {
     case ProcessBlogsView =>
-      val calendar = Calendar.getInstance()
-
-      calendar.add(Calendar.MONTH, -2)
-      calendar.set(Calendar.DATE, 1)
-
-      val firstDay = calendar.getTime
-
-      calendar.add(Calendar.MONTH, 1)
-      calendar.set(Calendar.DATE, 1)
-
-      val lastDay = calendar.getTime
-
-      val format = new SimpleDateFormat("yyyy-MM-dd")
-      val formattedFirstDay = format.format(firstDay)
-      val formattedLastDay = format.format(lastDay)
-
       log.info("Received message Process Blog Views and going to getTotalPost function")
 
-      val totalPosts = wordpressService.getTotalPost(formattedFirstDay, formattedLastDay) // yyyy-MM-dd
+      val firstDay = CalendarHelper.getFirstDay
+      val lastDay = CalendarHelper.getLastDay
+      val totalPosts = wordpressService.getTotalPost(firstDay, lastDay)
 
       log.info("Posts = " + totalPosts.mkString("\n"))
 
@@ -54,41 +39,26 @@ class WordpressController(implicit val system: ActorSystem) extends Actor with A
           (post.author.ID, post.author.name)
         }
       }
-      val result = postByAuthorIds.map {
-        case ((authorId, authorName), posts) =>
-          val viewsCountAndBlogId = posts.map { blog =>
-            val viewsCount = wordpressService.getPostViewByPostId(blog.ID)
-            (blog.ID, viewsCount)
-          }
-          val totalViews = viewsCountAndBlogId.map { case (_, views) => views }.sum
-          val blogIds = viewsCountAndBlogId.map { case (blogId, _) => blogId }
-          val viewCounts = viewsCountAndBlogId.map { case (_, views) => views }
-          Blogger(blogIds, authorId, authorName, blogIds.size, viewCounts, totalViews)
-      }.toList
 
-      val finalResult = result.sortBy(_.totalViews).reverse
+      val finalResult = getFinalResult(postByAuthorIds)
 
       log.info("total number of blogs in a month" + totalPosts.size)
       log.info("result" + finalResult.mkString("\n"))
 
-      val client = SlackApiClient(SLACK_API_TOKEN)
-      val eventualChannels = client.listChannels()
+      postMessageOnSlack(totalPosts, finalResult)
+  }
 
-      val eventualChannelNameToId = eventualChannels.map { channels =>
-        channels.map { channel =>
-          channel.name -> channel.id
-        }.toMap
-      }
+  def postMessageOnSlack(totalPosts: List[Post], finalResult: List[Blogger]): Future[String] = {
+    val client = SlackApiClient(SLACK_API_TOKEN)
 
-      val eventualMaybeChanId = eventualChannelNameToId.map { channelNameToId =>
-        channelNameToId.get(CHANNEL_NAME)
-      }
+    val eventualMaybeChanId = client.listChannels().map { channels =>
+      channels.map { channel =>
+        channel.name -> channel.id
+      }.toMap.get(CHANNEL_NAME)
+    }
 
-      val eventualChanId = eventualMaybeChanId.map { maybeChanId =>
-        maybeChanId.fold("")(identity)
-      }
-
-      eventualChanId.map { chanId =>
+    eventualMaybeChanId flatMap { maybeChanId =>
+      maybeChanId.fold(Future.successful("")) { chanId =>
         val index = 1 to totalPosts.size
 
         val formattedFinalResult = finalResult.map { blogger =>
@@ -105,5 +75,22 @@ class WordpressController(implicit val system: ActorSystem) extends Actor with A
 
         client.postChatMessage(chanId, formattedBlogsWithIndex mkString "\n", Some("Bot"))
       }
+    }
+  }
+
+  def getFinalResult(postByAuthorIds: Map[(Int, String), List[Post]]): List[Blogger] = {
+    val result = postByAuthorIds.map {
+      case ((authorId, authorName), posts) =>
+        val viewsCountAndBlogId = posts.map { blog =>
+          val viewsCount = wordpressService.getPostViewByPostId(blog.ID)
+          (blog.ID, viewsCount)
+        }
+        val totalViews = viewsCountAndBlogId.map { case (_, views) => views }.sum
+        val blogIds = viewsCountAndBlogId.map { case (blogId, _) => blogId }
+        val viewCounts = viewsCountAndBlogId.map { case (_, views) => views }
+        Blogger(blogIds, authorId, authorName, blogIds.size, viewCounts, totalViews)
+    }.toList
+
+    result.sortBy(_.totalViews).reverse
   }
 }
